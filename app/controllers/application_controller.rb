feea49eda28e8afa0da63e1e237ad44c5a26f7fe
ryogo asada <hello.teikoku@gmail.com>
2020-04-14 01:35:22 +0900

    ユーザー編集画面 ユーザー詳細画面 ユーザー一覧画面の調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: %i(name comment profile image))
+    end
+
+    def after_sign_in_path_for(resource)
+      users_path
     end
 end
