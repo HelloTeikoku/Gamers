0ec55f9e94c422371836b02b92da20d613aa72eb
ryogo asada <hello.teikoku@gmail.com>
2020-05-13 01:55:40 +0900

    メッセージコントローラー、ルームコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show]
   resources :favorites, only: %i[index create destroy]
+  resources :messages, only: %i[create]
+  resources :rooms, only: %i[create show]
 end
